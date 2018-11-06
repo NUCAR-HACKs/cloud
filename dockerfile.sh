6d2956f34b4b1288937ba0587dcb5b0886395003
jbooth <jason_booth@intuit.com>
2018-11-05 22:15:21 -0800

    Got OpenMC starting

--- dockerfile.sh
+++ dockerfile.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
 #MPI Install
-
+cd /shared/home/ccuser
+source env.sh
 if ! type "mpicc" > /dev/null; then
-  echo "**** first env"
-  sudo env
-  echo "**** second env"
-  sudo -E env
-  sudo apt-get install build-essential -y
-  sudo apt-get install wget -y
+  sudo apt-get install build-essential wget -y
   wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.3.tar.gz
-  sudo tar -xvzf openmpi-3.1.3.tar.gz
+  tar -xvzf openmpi-3.1.3.tar.gz
+  rm openmpi-3.1.3.tar.gz
   sudo mv openmpi-3.1.3 /usr/lib/
+  cd /usr/lib/
   cd /usr/lib/openmpi-3.1.3
-  sudo -E ./configure --prefix=/usr/lib/openmpi-3.1.3
+  ./configure --prefix=/usr/lib/openmpi-3.1.3
   sudo -E make all install
-  export PATH=/usr/lib/openmpi-3.1.3/bin:$PATH
-  echo "**** after install"
-  type "mpicc" > /dev/null
 fi
 
-echo "**** mpi test"
-which mpicc
-# Install dependencies from Debian package manager
 # Removed mpi related packages
 sudo apt-get update -y &&
 sudo apt-get upgrade -y &&
@@ -48,21 +40,17 @@ echo "**** pip test"
 pip --version
 pip3 --version
 sudo pip3 install --upgrade pip
-
 sudo pip install six NumPy SciPy pandas h5py Matplotlib uncertainties lxml
 
 sudo rm -rf /opt/NJOY2016 /opt/openmc
-
 # Clone and install NJOY2016
 sudo mkdir /opt/NJOY2016
 sudo git clone https://github.com/njoy/NJOY2016 /opt/NJOY2016
-
 cd /opt/NJOY2016
-
-sudo mkdir build 
-cd build 
-sudo -E cmake -Dstatic=on -DCMAKE_CXX_COMPILER=/usr/bin/mpicxx -DCMAKE_CC_COMPILER=/usr/bin/mpicc -DCMAKE_FC_COMPILER=/usr/bin/mpif90 ..
-sudo -E make 2>/dev/null 
+sudo mkdir build
+cd build
+sudo -E cmake -Dstatic=on ..
+sudo -E make 2>/dev/null
 sudo -E make install
 
 # Clone and install OpenMC
@@ -72,21 +60,18 @@ cd /opt/openmc
 sudo git checkout master
 sudo mkdir -p build
 cd build
-sudo -E cmake -Dopenmp=on -Doptimize=on -DHDF5_PREFER_PARALLEL=on -DCMAKE_CXX_COMPILER=/usr/bin/mpicxx -DCMAKE_CC_COMPILER=/usr/bin/mpicc -DCMAKE_FC_COMPILER=/usr/bin/mpif90 .. 
-sudo -E make 
+sudo -E cmake -Dopenmp=on -Doptimize=on -DHDF5_PREFER_PARALLEL=on ..
+sudo -E make
 sudo -E make install
 cd ..
 
 # Download cross sections (NNDC and WMP) and ENDF data needed by test suite
-sudo wget -O nndc_hdf5.tar.xz $(cat /opt/openmc/.travis.yml | grep anl.box | awk '{print $2}')
-sudo tar xJvf nndc_hdf5.tar.xz
-export OPENMC_CROSS_SECTIONS=/opt/openmc/nndc_hdf5/cross_sections.xml
+sudo wget -O nndc_hdf5.tar.xz https://anl.box.com/shared/static/a0eflty17atnpd0pp7460exagr3nuhm7.xz
+sudo tar -xJvf nndc_hdf5.tar.xz
 sudo git clone --branch=master git://github.com/smharper/windowed_multipole_library.git wmp_lib
 sudo tar xzvf wmp_lib/multipole_lib.tar.gz
-export OPENMC_MULTIPOLE_LIBRARY=/opt/openmc/multipole_lib
-
 cd /opt/openmc/examples/xml/pincell
-                                                                                                                                                                                                
+
 
 #$ -cwd
 #$ -j y
